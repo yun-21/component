3d6c6c6e744cf53a3febd7c6475782fc1ed97905
shinjiyun <ranji12@naver.com>
2024-06-11 10:55:24 +0900

    객체 동적할당

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ const a = (name, age, job) => {
 // console.log(a());
 
 const b = a("신지윤",25,"백수"); //객체를 만들어주는 캡슐
-console.log(b);
+console.log("첫번째 b: ", b);
+b.address = '대전';
+console.log("두번째 b: ", b)
