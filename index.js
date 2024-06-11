37404e934ee3c016dbaff3c7d3b4a482c52c2e6d
shinjiyun <ranji12@naver.com>
2024-06-11 10:48:03 +0900

    객체를 만들어 주는 캡슐

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const a = () => {
+const a = (name, age, job) => {
   return{
-    name:"",
-    age:0,
-    job:""
+    name:name,
+    age:age,
+    job:job
   }
 }
 // console.log(a());
 
-const b = a();
+const b = a("신지윤",25,"백수"); //객체를 만들어주는 캡슐
 console.log(b);
