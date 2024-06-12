68bd91ad19bd8c6bd53e791da99d4571a44cb943
shinjiyun <ranji12@naver.com>
2024-06-12 10:44:18 +0900

    정적인 한계를 깨기 위해서 외부에 있는 객체 리턴받은 많이 쓰는 구조

--- work-3.js
+++ work-3.js
@@ -1,1 +1,14 @@
-console.log("work-3")
+const basicData = () => {
+  return {
+    name: "신지윤",
+    age: 25,
+    job: "물먹기"
+  }
+}
+const component = () => {
+  let obj = basicData();
+
+  const decision = () => { }
+
+  return `<html></html>`;
+}
