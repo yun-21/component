29fdf67b96b6c9f8f928d7a7ae26e5e6b42918b6
shinjiyun <ranji12@naver.com>
2024-06-12 11:42:57 +0900

    생성자함수 규격 만들어서 찍어내기

--- work-3.js
+++ work-3.js
@@ -1,14 +1,49 @@
-const basicData = () => {
-  return {
-    name: "신지윤",
-    age: 25,
-    job: "물먹기"
+class BasicData {
+  constructor(name,age,job){
+    this.name=name;
+    this.age = age;
+    this.job = job;
+  }
+  set name(value){
+    if(typeof(value)==='string'){
+      this._name=value
+    }
+    else{
+      console.log('이름이 문자열이 아닙니다.')
+    }
+  }
+  set age(value){
+    if(typeof(value)==='number'){
+      this._age=value
+    }
+    else{
+      console.log('나이가 숫자가 아닙니다.')
+    }
+  }
+  set job(value){
+    if(typeof(value)==='string'){
+      this._job=value
+    }else{
+      console.log('직업이 문자열이 아닙니다.')
+    }
   }
 }
 const component = () => {
-  let obj = basicData();
-
-  const decision = () => { }
-
-  return `<html></html>`;
+  let obj = new BasicData('공욱재',26,'미남');
+  const decision = () => {
+    if(obj instanceof BasicData===true){
+      return `<html>
+      <head>
+      <body>
+      <h1>이름 : ${obj._name}</h1>
+      <h2>나이 : ${obj._age}</h2>
+      <h3>직업 : ${obj._job}</h3>
+      </body></head></html>`
+    }
+    else{
+      throw new Error();
+    }
+  }
+  return decision()
 }
+console.log(component());
