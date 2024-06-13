301fc8eb02f0202f32098758c9be13f77b41cff4
shinjiyun <ranji12@naver.com>
2024-06-13 10:37:07 +0900

    객체안의 객체 test(fail)

--- jangboki.js
+++ jangboki.js
@@ -1,1 +1,29 @@
-console.log("장보기리스트")
+class MartList {
+  constructor(name,age,job,drink){
+    ho : {
+      this.name = name,
+      this.age = age,
+      this.job = job
+    }
+    no : {
+      this.snack = "과자",
+      this.ice = "아이스크림",
+      this.drink = drink
+    }
+  }
+  set name(value){
+    if(typeof(value)==='string'){
+      this._name = value;
+    }
+  }
+  get name(){
+    return this._name
+  }
+}
+
+const doIt = () => {
+  const martList = new MartList("신지윤",22,"마트관리자","토마토쥬스");
+  
+  return console.log(martList.ho)
+}
+console.log(doIt())
