8793ef1b42d78b289e48aa1eb9b1a3620b410ac1
jiyun <ranji12@naver.com>
2024-06-16 22:29:17 +0900

    나의 가방에 대한 생성자함수 연습

--- myBag.js
+++ myBag.js
@@ -40,5 +40,30 @@ class MyBag {
 
 const inBag = (book,note,lunch) =>{
     const myBag = new MyBag(book,note,lunch);
-
+    
+    const helper = () => {
+        if(book === '딥다이브'){
+            console.log("javascript를 공부하시나봐요")
+        }
+        else if(book === '노드'){
+            console.log("Node를 공부하시나봐요")
+        }
+        else if(book === 'git'){
+            console.log("git에 대해 공부하시나봐요")
+        }
+        else{
+            console.log(`${book}에 대해 공부하시나봐요`)
+        }
+    }
+    helper()
+    return myBag
 }
+
+const call = (callback) =>{
+    const result = inBag("딥다이브","아이패드","불고기덮밥")
+    callback(result,menu)
+}
+const menu = (result) =>{
+    console.log(result)
+}
+call((result, callback)=>callback(result,menu))
