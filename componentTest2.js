226296f7af4a4e928363c6a9baf73b7c5e991b04
jiyun <ranji12@naver.com>
2024-06-21 01:44:20 +0900

    chatGPT에게 component에 맞춰 잘 쓴 코드냐고 물어보니까 이 코드는 컴포넌트의 개념을 잘못 이해하고 있다고해서 수정할 것임

--- componentTest2.js
+++ componentTest2.js
@@ -5,3 +5,12 @@ const component = (id, name ,age) =>{
         age : age
     }
 }
+
+const render = () =>{
+    const a = component (1,"컴포넌",22);
+    return `
+    <h1>${a.id}</h1>
+    <h3>${a.name}</h3>
+    <h4>${a.age}</h4>`
+}
+console.log(render())
